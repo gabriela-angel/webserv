65ce4e097431917885c0236af689b95a87ca30d3
Gabriela Angel Silva <90701152+gabriela-angel@users.noreply.github.com>
2026-05-23 14:13:50 -0300

    Clean up ManagerConfig.hpp by removing comments
    Removed commented-out default server configuration examples.

--- include/config/ManagerConfig.hpp
+++ include/config/ManagerConfig.hpp
@@ -23,21 +23,3 @@ public:
 std::ostream& operator<<(std::ostream& os, const ManagerConfig& config);
 
 #endif
-
-// Quando você permite múltiplos servers, você precisa definir um default server por porta
-// Exemplo:
-// server {
-//     listen 8080;
-//     server_name example.com;
-// }
-// server {
-//     listen 8080;
-// }
-
-// Se vier:
-
-// GET / HTTP/1.1
-// Host: unknown.com
-
-// Quem responde?
-//  O primeiro server com listen 8080.
